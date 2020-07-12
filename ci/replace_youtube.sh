ce2cb3195dac0e6e99f70fbae90055786cd22d54
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-13 00:44:46 +0800

    so confused... but somehow my regexp broke.

--- ci/replace_youtube.sh
+++ ci/replace_youtube.sh
@@ -2,6 +2,7 @@
 Rscript make_video_lists.R
 
 rm -f files_to_reprocess.txt # delete the old file if it is there.
+touch files_to_reprocess.txt # make an empty file to avoid errors.
 
 while read vid; do
   # vid=`head -1 video_ids.txt` # FOR TESTING 
@@ -12,7 +13,7 @@ while read vid; do
   
   echo "Looking for youtube $youtube_id"
   search_dir=`ls ../tutorials | grep W${week}D${day}`
-  find ../tutorials/${search_dir} -name 'W${week}D${day}*ipynb' -maxdepth 0 -type f -exec grep -l "YouTubeVideo(id.*?${youtube_id}" {} \; > files_to_change
+  find ../tutorials/${search_dir}/ -name "W${week}D${day}*ipynb" -maxdepth 1 -type f -exec grep -l "YouTubeVideo(id.*${youtube_id}" {} \; > files_to_change
   # Use maxdepth to avoid processing student notebooks.
   
   # sed -i'.bak' 's/:/ /g' files_to_change
@@ -27,11 +28,11 @@ while read vid; do
     echo "Found $youtube_id in $f2c"
     # fname=`head -1 files_to_change` # FOR TESTING 
     fname="$f2c"
-    lineno=`grep -n "YouTubeVideo(id.*?${youtube_id}" "$fname" | awk '{print $1}' | sed 's/://'` # Need to recompute lineno since it changed.
+    lineno=`grep -n "YouTubeVideo(id.*${youtube_id}" "$fname" | awk '{print $1}' | sed 's/://'` # Need to recompute lineno since it changed.
     
     # Figure out if this code cell has an import line.
 
-    has_import=`grep -b5 "YouTubeVideo(id.*?${youtube_id}" "$fname" | grep "from IPython"`
+    has_import=`grep -b5 "YouTubeVideo(id.*${youtube_id}" "$fname" | grep "from IPython"`
     if [[ -z "${has_import}" ]]; then
       startline=${lineno}
     else
@@ -63,7 +64,7 @@ cat files_to_reprocess.txt | sort | uniq > unique_files
 
 while read f2p; do
    echo "Rerunning $f2p"
-  jupyter nbconvert --to notebook --inplace --execute "$f2p" --allow-errors
+  # jupyter nbconvert --to notebook --inplace --execute "$f2p" --allow-errors
   python process_notebooks.py "$f2p"
 
 done <unique_files
