f362a8bb2991802a4de48f91fd43afc5bb7d6168
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-12 22:55:40 +0800

    updated script.

--- ci/replace_youtube.sh
+++ ci/replace_youtube.sh
@@ -1,28 +1,36 @@
 #!/bin/zsh
 Rscript make_video_lists.R
 
+# Change all hardcoded Neuromatch refs to erlichlab.
+find .. -type f -exec sed -i.back_0 's,github\.com/NeuromatchAcademy/github\.com/erlichlab/g'
+
 while read vid; do
-  # vid="nvpHtzuZggg BV1mt4y1Q7Ls" # FOR TESTING
+  # vid=`head -1 video_ids.txt` # FOR TESTING 
   youtube_id=`echo $vid | awk '{print $1}'`
   bilibili_id=`echo $vid | awk '{print $2}'`
+  week=`echo $vid | awk '{print $3}'`
+  day=`echo $vid | awk '{print $4}'`
   
   echo "Looking for youtube $youtube_id"
-  
-  find .. -name '*ipynb' -type f -exec grep -l "YouTubeVideo(id=.${youtube_id}." {} \; > files_to_change
+  search_dir=`ls ../tutorials | grep W${week}D${day}`
+  find ../tutorials/${search_dir} -name 'W${week}D${day}*ipynb' -type f -exec grep -l "YouTubeVideo(id.*?${youtube_id}" {} \; > files_to_change
   # sed -i'.bak' 's/:/ /g' files_to_change
   # rm -f files_to_change.bak
 
-
+   if [[ -s files_to_change ]]; then
+      echo "Found $youtube_id in:"
+      cat files_to_change
+   fi
 
   while read f2c; do
     echo "Found $youtube_id in $f2c"
     # fname=`head -1 files_to_change` # FOR TESTING 
     fname="$f2c"
-    lineno=`grep -n "YouTubeVideo(id=.${youtube_id}." "$fname" | awk '{print $1}' | sed 's/://'` # Need to recompute lineno since it changed.
+    lineno=`grep -n "YouTubeVideo(id.*?${youtube_id}" "$fname" | awk '{print $1}' | sed 's/://'` # Need to recompute lineno since it changed.
     
     # Figure out if this code cell has an import line.
 
-    has_import=`grep -b5 "YouTubeVideo(id=.${youtube_id}." "$fname" | grep "from IPython"`
+    has_import=`grep -b5 "YouTubeVideo(id.*?${youtube_id}" "$fname" | grep "from IPython"`
     if [[ -z "${has_import}" ]]; then
       startline=${lineno}
     else
@@ -54,9 +62,10 @@ cat files_to_reprocess.txt | sort | uniq > unique_files
 
 while read f2p; do
    echo "Rerunning $f2p"
-  jupyter nbconvert --to notebook --inplace --execute "$f2p"
+  jupyter nbconvert --to notebook --inplace --execute "$f2p" --allow-errors
 done <unique_files
 
+find .. -name '*back_0' -delete
 find .. -name '*back_1' -delete
 find .. -name '*back_2' -delete
 # rm -f files_to_reprocess.txt
