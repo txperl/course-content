d993b6506242b9a5086d8a860e298fa215d70ab6
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-12 22:40:45 +0800

    Include day and week in video list

--- ci/make_video_lists.R
+++ ci/make_video_lists.R
@@ -16,5 +16,7 @@ extract_youtube_id = function(url)
 gs4_deauth()
 video_crew_url = "https://docs.google.com/spreadsheets/d/1vx2hxDEvmw23P5niUxTjeuHwdyjt_8W5d6lTfM0s-7g/edit#gid=14620051"
 g4df = read_sheet(video_crew_url, sheet = "Real NMA", skip=5)
-linkdf = g4df %>% select(`Bilibili Link`, `YouTube link`) %>% na.omit() %>% mutate(bilibili_id=extract_bili_id(`Bilibili Link`), youtube_id=extract_youtube_id(`YouTube link`))
-write.table(linkdf %>% select(youtube_id, bilibili_id), 'video_ids.txt', quote=F, row.names = F, col.names = F)
+linkdf = g4df %>% select(Week, Day, `Bilibili Link`, `YouTube link`) %>% na.omit() %>% 
+  mutate(bilibili_id=extract_bili_id(`Bilibili Link`), youtube_id=extract_youtube_id(`YouTube link`)) %>%
+  filter(str_starts(bilibili_id, 'BV'))
+write.table(linkdf %>% select(youtube_id, bilibili_id, Week, Day), 'video_ids.txt', quote=F, row.names = F, col.names = F)
