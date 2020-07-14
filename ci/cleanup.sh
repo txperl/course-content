acde958f4257798e02d8a5e8a93a6578be42b914
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-14 14:52:38 +0800

    new strategy for links

---
+++ ci/cleanup.sh
@@ -0,0 +1,8 @@
+
+find .. -regex '.*back_[1-9]$' -delete
+# rm -f files_to_reprocess.txt
+rm -f this_bilicode
+rm -f bilibili_ids.txt
+rm -f youtube_ids.txt
+rm -f files_to_change
+rm -f video_ids.txt
--- ci/replace_youtube.sh
+++ ci/replace_youtube.sh
@@ -1,10 +1,70 @@
 #!/bin/zsh
+
+find_line () # fine_line pattern filename
+{
+  file=$2
+  patt=$1
+  echo `grep -n "$patt" "$file" | cut -f1 -d:` # Need to recompute lineno since it changed.
+}
+
+replace_iframe_code ()
+{
+    fname=$1
+    youtube_id=$2
+    bilibili_id=$3
+    lineno=`find_line "YouTubeVideo(id.*${youtube_id}" "$fname"`
+    # Figure out if this code cell has an import line.
+    has_import=`grep -b5 "YouTubeVideo(id.*${youtube_id}" "$fname" | grep "from IPython"`
+    if [[ -z "${has_import}" ]]; then
+      startline=${lineno}
+    else
+      startline=`expr ${lineno} - 1`
+    fi
+
+    endline=`expr $lineno + 1`
+    insertline=`expr $startline - 1`
+    # # Delete the youtube lines
+    sed -i.back_1 -e "${startline},${endline}d" "$fname"    
+    # Make the bilibili text
+    sed "s/BILIID/$bilibili_id/" bilibili_code > this_bilicode
+    # Insert bilibili text 
+    sed  -i.back_2 "${insertline}r this_bilicode" ${fname}
+} # end replace_iframe_code
+
+replace_play_link ()
+{
+  fname=$1
+  youtube_id=$2
+  bilibili_id=$3
+  yt_patt="Video available at https://youtube\.com/watch?v=${youtube_id}"
+  bili_patt="Video available at https://www\.bilibili\.com/video/${bilibili_id}"
+  sed -i.back_3 "s,${yt_patt},${bili_patt}," "$fname"
+}
+
+
+replace_src_link ()
+{
+  fname=$1
+  youtube_id=$2
+  bilibili_id=$3
+  #        "            src=\"https://www.youtube.com/embed/wbZ60vdnoqw?fs=1\"\n",
+  #bili_src='"            src=\"https://player.bilibili.com/player.html?bvid='"$bilibili_id"'&page=1?fs=1\"\\n",'
+  yt_patt="www\.youtube\.com/embed/${youtube_id}"
+  bili_patt="player\.bilibili\.com/player\.html\?bvid=${bilibili_id}"
+
+  sed -i.back_3 "s,${yt_patt},${bili_patt}," "$fname"
+}
+
+
+
+# Get the link map from google sheets using this R script.
 Rscript make_video_lists.R
 
 rm -f files_to_reprocess.txt # delete the old file if it is there.
 touch files_to_reprocess.txt # make an empty file to avoid errors.
 
-while read vid; do
+while read vid; do # Go through each line of video_ids.txt
+
   # vid=`head -1 video_ids.txt` # FOR TESTING 
   youtube_id=`echo $vid | awk '{print $1}'`
   bilibili_id=`echo $vid | awk '{print $2}'`
@@ -13,11 +73,8 @@ while read vid; do
   
   echo "Looking for youtube $youtube_id"
   search_dir=`ls ../tutorials | grep W${week}D${day}`
-  find ../tutorials/${search_dir}/ -name "W${week}D${day}*ipynb" -maxdepth 1 -type f -exec grep -l "YouTubeVideo(id.*${youtube_id}" {} \; > files_to_change
+  find ../tutorials/${search_dir}/ -name "W${week}D${day}*ipynb" -type f -exec grep -l "YouTubeVideo(id.*${youtube_id}" {} \; > files_to_change
   # Use maxdepth to avoid processing student notebooks.
-  
-  # sed -i'.bak' 's/:/ /g' files_to_change
-  # rm -f files_to_change.bak
 
    if [[ -s files_to_change ]]; then
       echo "Found $youtube_id in:"
@@ -28,54 +85,26 @@ while read vid; do
     echo "Found $youtube_id in $f2c"
     # fname=`head -1 files_to_change` # FOR TESTING 
     fname="$f2c"
-    lineno=`grep -n "YouTubeVideo(id.*${youtube_id}" "$fname" | awk '{print $1}' | sed 's/://'` # Need to recompute lineno since it changed.
+    replace_iframe_code $fname $youtube_id $bilibili_id
+    replace_src_link $fname $youtube_id $bilibili_id
+    replace_play_link $fname $youtube_id $bilibili_id
+
     
-    # Figure out if this code cell has an import line.
-
-    has_import=`grep -b5 "YouTubeVideo(id.*${youtube_id}" "$fname" | grep "from IPython"`
-    if [[ -z "${has_import}" ]]; then
-      startline=${lineno}
-    else
-      startline=`expr ${lineno} - 1`
-    fi
-
-    endline=`expr $lineno + 1`
-    insertline=`expr $startline - 1`
-
     # save the files for later
-
     echo "$fname" >> files_to_reprocess.txt
 
-    # # Delete the youtube lines
-    sed -i.back_1 -e "${startline},${endline}d" "$fname"    
-  
-    # Make the bilibili text
-    sed "s/BILIID/$bilibili_id/" bilibili_code > this_bilicode
-       
-    # Insert bilibili text 
-    sed  -i.back_2 "${insertline}r this_bilicode" ${fname}
 
   done <files_to_change 
 
 done <video_ids.txt
 
-
+./replace_github_links.sh
 
 # Only need to rerun each notebook once!
 cat files_to_reprocess.txt | sort | uniq > unique_files
 
 while read f2p; do
-   echo "Rerunning $f2p"
-  jupyter nbconvert --to notebook --inplace --execute "$f2p" --allow-errors
+   echo "Changed $f2p"
+#  jupyter nbconvert --to notebook --inplace --execute "$f2p" --allow-errors
 done <unique_files
 
-
-find .. -name '*back_0' -delete
-find .. -name '*back_1' -delete
-find .. -name '*back_2' -delete
-# rm -f files_to_reprocess.txt
-rm -f this_bilicode
-rm -f bilibili_ids.txt
-rm -f youtube_ids.txt
-rm -f files_to_change
-rm -f video_ids.txt
